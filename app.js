f69db7870a8641ed2e37415c5c78272a8eb98446
sakib-git <sakibulhasan32002@gmail.com>
2025-07-29 18:43:00 +0600

    update

--- app.js
+++ app.js
@@ -1,18 +1,22 @@
-const nameM = document.getElementById('name')
-const age = document.getElementById('age')
-const number = document.getElementById('number')
-const btn = document.getElementById('btn')
-const result = document.getElementById('result')
-const Money = document.getElementById('Money')
+const nameM = document.getElementById('name');
+const age = document.getElementById('age');
+const number = document.getElementById('number');
+const btn = document.getElementById('btn');
+const result = document.getElementById('result');
+const Money = document.getElementById('Money');
+
+zeroBtn.zeroRipple('.ripple-effect-btn', {
+  color: 'skyblue',
+});
 
 
-const nameresult = document.getElementById('nameresult')
-const ageresult = document.getElementById('ageresult')
-const numberresult = document.getElementById('numberresult')
-const moneyresult = document.getElementById('moneyresult')
-const title = document.getElementById('title')
+
+const nameresult = document.getElementById('nameresult');
+const ageresult = document.getElementById('ageresult');
+const numberresult = document.getElementById('numberresult');
+const moneyresult = document.getElementById('moneyresult');
+const title = document.getElementById('title');
 btn.addEventListener('click', () => {
-
   const namevalue = nameM.value;
   nameresult.innerText = namevalue;
 
@@ -25,80 +29,67 @@ btn.addEventListener('click', () => {
   const moneyvalue = Money.value;
   moneyresult.innerText = moneyvalue;
 
-
-if (moneyvalue < 550) {
+  if (moneyvalue < 550) {
     moneyresult.innerText = 'Please pay 550';
-    title.innerText = 'Something wrong'
-}
-  else if(agevalue <= 12 ){
-    moneyresult.innerText = 'free'
-    title.innerText = 'you are Underage'
-  }
-  else if(agevalue >= 12 && agevalue <= 30 ){
-      const discount = moneyvalue * 30 / 100
-      const total = moneyvalue - discount
-      moneyresult.innerText = `${total} tk`;
-      title.innerText = 'you are a student, you got 30%  discount'
-  }
-  else if(agevalue >= 65 ){
-    const discount = moneyvalue * 15 / 100;
+    title.innerText = 'Something wrong';
+  } else if (agevalue <= 12) {
+    moneyresult.innerText = 'free';
+    title.innerText = 'you are Underage';
+  } else if (agevalue >= 12 && agevalue <= 30) {
+    const discount = (moneyvalue * 30) / 100;
+    const total = moneyvalue - discount;
+    moneyresult.innerText = `${total} tk`;
+    title.innerText = 'you are a student, you got 30%  discount';
+  } else if (agevalue >= 65) {
+    const discount = (moneyvalue * 15) / 100;
     const total = moneyvalue - discount;
     moneyresult.innerText = total;
-    title.innerText = 'You are an old citizen, you got 15% discount'
+    title.innerText = 'You are an old citizen, you got 15% discount';
+  } else {
+    moneyresult.innerText = moneyvalue;
+    title.innerText = 'Thank you';
   }
-  
-  else {
-      moneyresult.innerText = moneyvalue;
-      title.innerText = 'Thank you'
+
+  const container = document.querySelector('.container');
+  const result = document.getElementById('result');
+  const allinput = document.querySelectorAll('.allinput');
+
+  let allFilled = true;
+
+  allinput.forEach((i) => {
+    if (i.value == '') {
+      i.classList.add('show');
+      allFilled = false;
     }
 
-   const container = document.querySelector('.container')
-    const result = document.getElementById('result')
- const allinput = document.querySelectorAll('.allinput')
-
- let allFilled = true;
-
-    allinput.forEach((i) => {
-    if(i.value == ''){
-    i.classList.add('show')
-       allFilled = false;
-  } 
-  
-    if (allFilled) //or if(i.value !== '')
-     {
-    container.style.display = 'none';
-    result.style.display = 'block';
-  } 
-
-})
-
-
-allinput.forEach((i) => {
-  i.addEventListener('keyup', () => {
-    if(i.value !== ''){
-      i.classList.remove('show')
+    if (allFilled) {
+      //or if(i.value !== '')
+      container.style.display = 'none';
+      result.style.display = 'block';
     }
-  })
-})
+  });
 
-number.addEventListener('keyup', () => {
-    if(number.value.length < 11){
-      number.classList.add('show')
+  allinput.forEach((i) => {
+    i.addEventListener('keyup', () => {
+      if (i.value !== '') {
+        i.classList.remove('show');
+      }
+    });
+  });
+
+  number.addEventListener('keyup', () => {
+    if (number.value.length < 11) {
+      number.classList.add('show');
+    } else {
+      number.classList.remove('show');
     }
-    else{
-        number.classList.remove('show')
-    }
+  });
+});
 
-})
- 
-  
-})
-
-
-const getAnotherBtn = document.getElementById('get-another-btn')
-const container = document.querySelector('.container')
+const getAnotherBtn = document.getElementById('get-another-btn');
+const container = document.querySelector('.container');
 
 getAnotherBtn.addEventListener('click', () => {
-  result.style.display = 'none'
-  container.style.display = 'flex'
-})
+  result.style.display = 'none';
+  container.style.display = 'flex';
+});
